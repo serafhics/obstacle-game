253457d24e896972b1989b25c1241395650e1ce2
serafhics <139830709+serafhics@users.noreply.github.com>
2023-11-11 08:52:40 +0530

    Ar vr

---
+++ Web build/ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "21VOA45-Obstacle_course-0.1";
+const contentToCache = [
+    "Build/Web build.loader.js",
+    "Build/Web build.framework.js",
+    "Build/Web build.data",
+    "Build/Web build.wasm",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
